0ce5c9d87bd84d7efe010bb0ed622991bc5d5df0
Sha <Sha@LAPTOP-SSEOA9CB>
2022-08-14 21:01:30 +0530

    "Initial Commit"

---
+++ Java/src/JavaPractice/Sample.java
@@ -0,0 +1,14 @@
+package JavaPractice;
+
+public class Sample {
+	
+	public void method() {
+		System.out.println("Sample Testing");
+	}
+	
+	public static void main(String[] args) {
+		Sample ob=new Sample();
+		ob.method();
+	}
+
+}
